8a2f7a700f8d92295fd99cc06ba217e58233064f
samuelbergen <kontakt@samuel-bergen.de>
2022-06-04 21:18:51 +0200

    first steps

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { User } from 'src/models/user.class';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+
+  user: User = new User();
+
+  constructor(public dialog: MatDialog) { 
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+  openAddUserDialog() {
+    this.dialog.open(DialogAddUserComponent);
+  }
+}
